50425aea202f4545c993ea0524634cc5035cb801
Gyula Soos <gyula.soos@aerian.com>
2021-07-28 17:00:55 +0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "gsc-calculator",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-gatsby-cloud"],
+};
